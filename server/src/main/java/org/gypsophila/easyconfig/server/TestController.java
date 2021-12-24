f307ffd4d49e8bb2543e694a041262e680ebb05f
lixiaoshuang <644968328@qq.com>
2021-12-24 17:45:17 +0800

    初始化工程

--- server/src/main/java/org/gypsophila/easyconfig/server/TestController.java
+++ server/src/main/java/org/gypsophila/easyconfig/server/TestController.java
@@ -21,9 +21,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Map;
-
 /**
  * @author lixiaoshuang
  */
